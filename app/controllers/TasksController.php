d049d676893f5a9fd3a1874c4314c50ee7eeab39
lehools <francoisdarbellay@bluewin.ch>
2014-04-08 17:06:25 +0000

    input all

--- app/controllers/TasksController.php
+++ app/controllers/TasksController.php
@@ -8,9 +8,9 @@ class TasksController extends BaseController {
 	}
 	public function store()
 	{
-    Tasks::create(Input::post());
+    //Tasks::create(Input::post());
     //Tasks::create(Input::get());
-    //Tasks::create(Input::all()); //enregistre le formulaire dans la base de donnée
+    Tasks::create(Input::all()); //enregistre le formulaire dans la base de donnée
     //return Redirect::route('tasks.create');
    
     return Redirect::route('tasks_create');
